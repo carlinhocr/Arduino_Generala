ff6e57c0765ff7680b7aa93e898064a81e8c2dbd
carlinhocr <carlos.rienzi@gmail.com>
2023-05-11 15:40:34 -0300

    threedice

--- ThreeDice/ThreeDice.ino
+++ ThreeDice/ThreeDice.ino
@@ -3,14 +3,16 @@
 int _buttonGeneral = 2;
 int _diceButton1=11;
 int _diceButton2=12;
-int _iterateDice[3] = {1,1,1}; //1, 2
-int _randomDiceNumber[3] = {0,0.0}; //1, 2
-int _buttonStopDicePin[3] = {11,12,12};
+int _diceButton3=20;
+const int _diceTotal = 3;
+int _iterateDice[_diceTotal] = {1,1,1}; //1, 2
+int _randomDiceNumber[_diceTotal] = {0,0.0}; //1, 2
+int _buttonStopDicePin[_diceTotal] = {11,12,12};
 
-MyDice dice1(3,4,5,6);
-MyDice dice2(7,8,9,10);
-MyDice dice3(A0,A1,A2,A3);
-MyDice _AllDice[3] = {dice1,dice2,dice3};
+MyDice dice1(47,49,51,53);
+MyDice dice2(39,41,43,45);
+MyDice dice3(31,33,35,37);
+MyDice _AllDice[_diceTotal] = {dice1,dice2,dice3};
 
 void acknowledgeButtonPress(int diceNumber){
   _AllDice[diceNumber].flashAllLeds(2,200);
@@ -18,7 +20,7 @@ void acknowledgeButtonPress(int diceNumber){
 }
 
 void checkDiceStopStartButton(){
-    for  (int diceNumber = 0; diceNumber < 2; diceNumber ++){ // maybe use len(_AllDice) instead of two
+    for  (int diceNumber = 0; diceNumber < _diceTotal; diceNumber ++){ // maybe use len(_AllDice) instead of two
       if (digitalRead(_buttonStopDicePin[diceNumber]) == LOW){
         if (_iterateDice[diceNumber] == 1){
           _iterateDice[diceNumber] = 0 ; // 0 means do not roll leave as is    
@@ -46,14 +48,14 @@ void checkButton() {
 }
 
 void iterateNumbers(){
-  for  (int diceNumber = 0; diceNumber < 2; diceNumber ++){
+  for  (int diceNumber = 0; diceNumber < _diceTotal; diceNumber ++){
     if (_iterateDice[diceNumber] == 1){
       _AllDice[diceNumber].turnOffDice();
       delay(300);
     };
   };
   for (int position = 1; position < 7; position++) {
-    for  (int diceNumber = 0; diceNumber < 2; diceNumber ++){
+    for  (int diceNumber = 0; diceNumber < _diceTotal; diceNumber ++){
       if (_iterateDice[diceNumber] == 1){
           _AllDice[diceNumber].lightNumber(position);
           delay(50);
@@ -65,7 +67,7 @@ void iterateNumbers(){
 }
 
 void assignRandomNumber(){
-  for  (int diceNumber = 0; diceNumber < 2; diceNumber ++){
+  for  (int diceNumber = 0; diceNumber < _diceTotal; diceNumber ++){
     if (_iterateDice[diceNumber] == 1){
       _randomDiceNumber[diceNumber] = random(1,7);
     };
@@ -73,7 +75,7 @@ void assignRandomNumber(){
 }
 
 void lightDiceRamdomNumber(){
-  for  (int diceNumber = 0; diceNumber < 2; diceNumber ++){
+  for  (int diceNumber = 0; diceNumber < _diceTotal; diceNumber ++){
     if (_iterateDice[diceNumber] == 1){
       _AllDice[diceNumber].lightNumber(_randomDiceNumber[diceNumber]);
       Serial.println(_randomDiceNumber[diceNumber]);
@@ -104,12 +106,13 @@ void oneDiceWithButton(){
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(A0,OUTPUT);
-  digitalWrite(A0,HIGH);
+  Serial.begin(9600);
+  Serial.println("En funcion setup");
   pinMode(_buttonGeneral, INPUT_PULLUP);
   pinMode(_diceButton1, INPUT_PULLUP);
   pinMode(_diceButton2, INPUT_PULLUP);
-  Serial.begin(9600);
+  ;pinMode(_diceButton3, INPUT_PULLUP);
+
 }
 
 void loop() {
